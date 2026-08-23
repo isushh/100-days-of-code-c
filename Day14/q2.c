a245cf1447d39363056d993c01041f7f1910c1c2
isushh <sushant825145@gmail.com>
2026-08-23 23:32:30 +0530

    Day14

---
+++ Day14/q2.c
@@ -0,0 +1,20 @@
+/*
+Q28: Write a program to print the product of even numbers from 1 to n.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, i, product = 1;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i = i + 2) {
+        product = product * i;
+    }
+
+    printf("Product = %d", product);
+
+    return 0;
+}
